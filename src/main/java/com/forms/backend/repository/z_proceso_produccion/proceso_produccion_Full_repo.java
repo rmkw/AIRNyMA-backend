120019ddbed07ad424c1549d38511fd2af6cd3ad
rmkw <creditozona1@gmail.com>
2025-04-22 16:10:21 -0600

    delete progress z_proceso change z_direccion

--- src/main/java/com/forms/backend/repository/z_proceso_produccion/proceso_produccion_Full_repo.java
+++
@@ -1,9 +0,0 @@
-package com.forms.backend.repository.z_proceso_produccion;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.forms.backend.entitys.z_procesos_produccion.proceso_produccion_FULL;
-
-public interface proceso_produccion_Full_repo extends JpaRepository<proceso_produccion_FULL, Long>{
-    
-}
