9bafadda431717c2862b3f94c404f21f2e77cc6d
rmkw <creditozona1@gmail.com>
2025-04-22 16:23:33 -0600

    add repo direcciones

---
+++ src/main/java/com/forms/backend/services/z_direcciones/service_direccionesGenerales.java
@@ -0,0 +1,20 @@
+package com.forms.backend.services.z_direcciones;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.forms.backend.entitys.z_direcciones.Enty_direccionesGenerales;
+import com.forms.backend.repository.z_direcciones.repo_direccionesGenerales;
+
+@Service
+public class service_direccionesGenerales {
+
+    @Autowired
+    private repo_direccionesGenerales repo;
+
+    public List<Enty_direccionesGenerales> getAllDir(){
+        return repo.findAll();
+    }
+}
