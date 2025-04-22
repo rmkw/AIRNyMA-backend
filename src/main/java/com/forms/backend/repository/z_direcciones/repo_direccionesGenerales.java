cca5faa641a0cd433857f65e3a73d633af2d9f50
rmkw <creditozona1@gmail.com>
2025-04-22 16:18:46 -0600

    add repo direcciones generales

---
+++ src/main/java/com/forms/backend/repository/z_direcciones/repo_direccionesGenerales.java
@@ -0,0 +1,9 @@
+package com.forms.backend.repository.z_direcciones;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.forms.backend.entitys.z_direcciones.Enty_direccionesGenerales;
+
+public interface repo_direccionesGenerales extends JpaRepository<Enty_direccionesGenerales, Long>{
+    
+}
